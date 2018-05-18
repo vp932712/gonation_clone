c82fe30f6c418592e0da30def088ba9330d6e832
Vic Patel <patel92vaibhav@gmail.com>
2018-05-18 13:57:39 -0400

    0.0.3

--- src/Footer.js
+++ src/Footer.js
@@ -14,13 +14,13 @@ render() {
       <a class = "SiteFooter__Nav__Links__Link">FAQ</a>
     </div>
     <div class="SiteFooter__Nav__SocialMobileContainer">
-      <div >
+      <div class="SiteFooter__Nav__SocialMobileContainer"  >
       <a class="SiteFooter__Nav__SocialMobileContainer__Icons__Icon"> F</a>
       <a class="SiteFooter__Nav__SocialMobileContainer__Icons__Icon">G </a>
       <a class="SiteFooter__Nav__SocialMobileContainer__Icons__Icon">L </a>
       <a class="SiteFooter__Nav__SocialMobileContainer__Icons__Icon">M </a>
       </div>
-      <div >
+      <div class="SiteFooter__Nav__SocialMobileContainer" >
       <a class="SiteFooter__Nav__SocialMobileContainer__MobileLinks__Link--apple"></a>
       <a class="SiteFooter__Nav__SocialMobileContainer__MobileLinks__Link--google">Android</a>
       </div>
