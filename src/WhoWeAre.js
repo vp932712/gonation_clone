0b6c5c258a84824dc74472d6fe12899e4e3d969a
Vic Patel <patel92vaibhav@gmail.com>
2018-05-19 18:05:07 -0400

    0.1.2

--- src/WhoWeAre.js
+++ src/WhoWeAre.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import "./WhoWeAre.css"
+import WhoWeAreReasons from "./WhoWeAreReasons.js"
 
 
 class WhoWeAre extends Component {
@@ -7,7 +8,10 @@ class WhoWeAre extends Component {
 render() {
   return (
     <div>
-      <div class="">
+      <div class="WhyOrderPanel">
+        <h2 class="Light">What is GoNation </h2>
+        <div class="HomepageDivider"> </div>
+        <WhoWeAreReasons />
       </div>
     </div>
   )
---
+++ src/WhoWeAreReasons.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+
+
+class WhoWeAreReasons extends Component {
+
+render() {
+  return (
+    <div>
+      <div class="WhoWeArePanel">
+        <div class="Bubbles__First">
+          <span class="First__Bubble"> </span>
+          <span class="Second__Bubble"> </span>
+          <span class="Third__Bubble"> </span>
+          <span class="Fourth__Bubble"> </span>
+          <span class="Fifth__Bubble"> </span>
+          <span class="Sixth__Bubble"> </span>
+          <span class="Seventh__Bubble"> </span>
+          <span class="Eighth__Bubble"> </span>
+          <span class="Nineth__Bubble"> </span>
+        </div>
+        <div class="Bubbles__Second">
+          <span class="First__Bubble"> </span>
+          <span class="Second__Bubble"> </span>
+          <span class="Third__Bubble"> </span>
+          <span class="Fourth__Bubble"> </span>
+          <span class="Fifth__Bubble"> </span>
+          <span class="Sixth__Bubble"> </span>
+          <span class="Seventh__Bubble"> </span>
+          <span class="Eighth__Bubble"> </span>
+          <span class="Nineth__Bubble"> </span>
+        </div>
+        <div>
+          <div>
+            <div>
+              <img/>
+            </div>
+            <h3> </h3>
+            <p> </p>
+          </div>
+          <div>
+            <div>
+              <img/>
+            </div>
+            <h3> </h3>
+            <p> </p>
+          </div>
+          <div>
+            <div>
+              <img/>
+            </div>
+            <h3> </h3>
+            <p> </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+}
+
+
+export default WhoWeAreReasons
