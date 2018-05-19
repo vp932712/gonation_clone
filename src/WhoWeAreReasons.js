8da268fc7a5aa7c686489f1d5596ccbbb3fabe95
Vic Patel <patel92vaibhav@gmail.com>
2018-05-19 19:00:51 -0400

    0.1.3

--- src/WhoWeAreReasons.js
+++ src/WhoWeAreReasons.js
@@ -6,7 +6,7 @@ class WhoWeAreReasons extends Component {
 
 render() {
   return (
-    <div>
+
       <div class="WhoWeArePanel">
         <div class="Bubbles__First">
           <span class="First__Bubble"> </span>
@@ -30,31 +30,31 @@ render() {
           <span class="Eighth__Bubble"> </span>
           <span class="Nineth__Bubble"> </span>
         </div>
-        <div>
-          <div>
-            <div>
-              <img/>
+        <div class="WhyOrderPanel__Reasons">
+          <div class="WhyOrderPanel__Reasons__Reason__Discover">
+            <div class="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
+              <img class="Discover__Image" src="https://smart.servier.com/wp-content/uploads/2016/10/ECGentier5.png"/>
             </div>
-            <h3> </h3>
-            <p> </p>
+            <h3 class="WhyOrderPanel__Reasons__Reason__Headline">DISCOVER</h3>
+            <p class="P">Know where the amazing things are in your city right now.</p>
           </div>
-          <div>
-            <div>
-              <img/>
+          <div class="WhyOrderPanel__Reasons__Reason__Discover">
+            <div class="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
+              <img class="Discover__Image" src="https://www.cajabancaria.gov.py/v4/templates/rt_supra/images/home/showcase/magnifier-1724298_960_720.png"/>
             </div>
-            <h3> </h3>
-            <p> </p>
+            <h3 class="WhyOrderPanel__Reasons__Reason__Headline">EXPLORE</h3>
+            <p class="P">Uncover what you desire in the city with an advanced real-time local search.</p>
           </div>
-          <div>
-            <div>
-              <img/>
+          <div class="WhyOrderPanel__Reasons__Reason__Discover">
+            <div class="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
+              <img class="Discover__Image" src="https://s3.amazonaws.com/coursestorm/live/media/872c79e0efe311e7987212b5ca500252"/>
             </div>
-            <h3> </h3>
-            <p> </p>
+            <h3 class="WhyOrderPanel__Reasons__Reason__Headline">SHARE</h3>
+            <p class="P">Inspire others with your experience and help them discover what the area has to offer.</p>
           </div>
         </div>
       </div>
-    </div>
+
   )
 }
 
