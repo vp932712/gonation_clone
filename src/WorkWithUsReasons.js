8547700725207e032e19aca264afcf73099f326e
Vic Patel <patel92vaibhav@gmail.com>
2018-05-19 20:28:21 -0400

    0.1.5

--- src/WorkWithUsReasons.js
+++ src/WorkWithUsReasons.js
@@ -7,8 +7,66 @@ class WorkWithUsReasons extends Component {
 render() {
   return (
     <div>
+   <div class="GetToKnowUsPanel__Blocks">
+      <div class="dcom__container">
+        <div class="PanelBlock__First">
+          <div class="PanelBlock__ImageWrap__First">
+            <div class="PanelBlock__ImageWrap__Image__First">
+            </div>
+          </div>
+          <div class="PanelBlock__Content__First">
+            <div class="PanelBlock__Content__Inner">
+              <h3 class="PanelBlock__Content__Headline">Share Your Business</h3>
+              <p class="P__P">Being discovered has never been so simple!</p>
+              <a class="Button__Button">Learn More</a>
+              </div>
+          </div>
+        </div>
+      </div>
 
-    </div>
+
+   </div>
+   <div class="GetToKnowUsPanel__Blocks">
+      <div class="dcom__container">
+        <div class="PanelBlock__First">
+          <div class="PanelBlock__ImageWrap__First">
+            <div class="PanelBlock__ImageWrap__Image__First">
+            </div>
+          </div>
+          <div class="PanelBlock__Content__First">
+            <div class="PanelBlock__Content__Inner">
+              <h3 class="PanelBlock__Content__Headline">About Us</h3>
+              <p class="P__P">We are passionate team of creatives, engineers and growth hackers!</p>
+              <a class="Button__Button">Learn More</a>
+              </div>
+          </div>
+        </div>
+      </div>
+
+
+   </div>
+   <div class="GetToKnowUsPanel__Blocks">
+      <div class="dcom__container">
+        <div class="PanelBlock__First">
+          <div class="PanelBlock__ImageWrap__First">
+            <div class="PanelBlock__ImageWrap__Image__First">
+            </div>
+          </div>
+          <div class="PanelBlock__Content__First">
+            <div class="PanelBlock__Content__Inner">
+              <h3 class="PanelBlock__Content__Headline">Our Mission</h3>
+              <p class="P__P">At GoNation we believe life's too short to miss out. We believe in better local discovery!</p>
+              <a class="Button__Button">Learn More</a>
+              </div>
+          </div>
+        </div>
+      </div>
+
+
+   </div>
+   </div>
+
+
   )
 }
 
