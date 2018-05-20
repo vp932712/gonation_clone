1921b0a8cb6ef166d990a7a62a9eb73a0aca423b
Vic Patel <patel92vaibhav@gmail.com>
2018-05-20 16:00:52 -0400

    0.3.1

--- src/WhoWeAreReasons.js
+++ src/WhoWeAreReasons.js
@@ -7,50 +7,50 @@ class WhoWeAreReasons extends Component {
 render() {
   return (
 
-      <div class="WhoWeArePanel">
-        <div class="Bubbles__First">
-          <span class="First__Bubble"> </span>
-          <span class="Second__Bubble"> </span>
-          <span class="Third__Bubble"> </span>
-          <span class="Fourth__Bubble"> </span>
-          <span class="Fifth__Bubble"> </span>
-          <span class="Sixth__Bubble"> </span>
-          <span class="Seventh__Bubble"> </span>
-          <span class="Eighth__Bubble"> </span>
-          <span class="Nineth__Bubble"> </span>
+      <div className="WhoWeArePanel">
+        <div className="Bubbles__First">
+          <span className="First__Bubble"> </span>
+          <span className="Second__Bubble"> </span>
+          <span className="Third__Bubble"> </span>
+          <span className="Fourth__Bubble"> </span>
+          <span className="Fifth__Bubble"> </span>
+          <span className="Sixth__Bubble"> </span>
+          <span className="Seventh__Bubble"> </span>
+          <span className="Eighth__Bubble"> </span>
+          <span className="Nineth__Bubble"> </span>
         </div>
-        <div class="Bubbles__Second">
-          <span class="First__Bubble"> </span>
-          <span class="Second__Bubble"> </span>
-          <span class="Third__Bubble"> </span>
-          <span class="Fourth__Bubble"> </span>
-          <span class="Fifth__Bubble"> </span>
-          <span class="Sixth__Bubble"> </span>
-          <span class="Seventh__Bubble"> </span>
-          <span class="Eighth__Bubble"> </span>
-          <span class="Nineth__Bubble"> </span>
+        <div className="Bubbles__Second">
+          <span className="First__Bubble"> </span>
+          <span className="Second__Bubble"> </span>
+          <span className="Third__Bubble"> </span>
+          <span className="Fourth__Bubble"> </span>
+          <span className="Fifth__Bubble"> </span>
+          <span className="Sixth__Bubble"> </span>
+          <span className="Seventh__Bubble"> </span>
+          <span className="Eighth__Bubble"> </span>
+          <span className="Nineth__Bubble"> </span>
         </div>
-        <div class="WhyOrderPanel__Reasons">
-          <div class="WhyOrderPanel__Reasons__Reason__Discover">
-            <div class="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
-              <img class="Discover__Image" src="https://smart.servier.com/wp-content/uploads/2016/10/ECGentier5.png"/>
+        <div className="WhyOrderPanel__Reasons">
+          <div className="WhyOrderPanel__Reasons__Reason__Discover">
+            <div className="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
+              <img className="Discover__Image" src="https://smart.servier.com/wp-content/uploads/2016/10/ECGentier5.png"/>
             </div>
-            <h3 class="WhyOrderPanel__Reasons__Reason__Headline">DISCOVER</h3>
-            <p class="P">Know where the amazing things are in your city right now.</p>
+            <h3 className="WhyOrderPanel__Reasons__Reason__Headline">DISCOVER</h3>
+            <p className="P">Know where the amazing things are in your city right now.</p>
           </div>
-          <div class="WhyOrderPanel__Reasons__Reason__Discover">
-            <div class="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
-              <img class="Discover__Image" src="https://www.cajabancaria.gov.py/v4/templates/rt_supra/images/home/showcase/magnifier-1724298_960_720.png"/>
+          <div className="WhyOrderPanel__Reasons__Reason__Discover">
+            <div className="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
+              <img className="Discover__Image" src="https://www.cajabancaria.gov.py/v4/templates/rt_supra/images/home/showcase/magnifier-1724298_960_720.png"/>
             </div>
-            <h3 class="WhyOrderPanel__Reasons__Reason__Headline">EXPLORE</h3>
-            <p class="P">Uncover what you desire in the city with an advanced real-time local search.</p>
+            <h3 className="WhyOrderPanel__Reasons__Reason__Headline">EXPLORE</h3>
+            <p className="P">Uncover what you desire in the city with an advanced real-time local search.</p>
           </div>
-          <div class="WhyOrderPanel__Reasons__Reason__Discover">
-            <div class="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
-              <img class="Discover__Image" src="https://s3.amazonaws.com/coursestorm/live/media/872c79e0efe311e7987212b5ca500252"/>
+          <div className="WhyOrderPanel__Reasons__Reason__Discover">
+            <div className="WhyOrderPanel__Reasons__Reason__Discover__ImageWrap">
+              <img className="Discover__Image" src="https://s3.amazonaws.com/coursestorm/live/media/872c79e0efe311e7987212b5ca500252"/>
             </div>
-            <h3 class="WhyOrderPanel__Reasons__Reason__Headline">SHARE</h3>
-            <p class="P">Inspire others with your experience and help them discover what the area has to offer.</p>
+            <h3 className="WhyOrderPanel__Reasons__Reason__Headline">SHARE</h3>
+            <p className="P">Inspire others with your experience and help them discover what the area has to offer.</p>
           </div>
         </div>
       </div>
