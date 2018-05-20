98bce6cc5e3b34ec0eb585185deed6213c490977
Vic Patel <patel92vaibhav@gmail.com>
2018-05-20 15:59:00 -0400

    0.2.6

--- src/DiscoverSearchBox.js
+++ src/DiscoverSearchBox.js
@@ -7,21 +7,21 @@ class DiscoverSearchBox extends Component {
 render() {
   return (
     <div>
-      <div class="SiteSearchPanel__SearchBlock">
+      <div className="SiteSearchPanel__SearchBlock">
 
-        <div class="SiteSearchPanel__SearchBlock__Inner">
-          <div class="SiteSearchPanel__SearchBlock__OpacityCover"> </div>
-          <div class="SiteSearchPanel__SearchBlock__OpacityContainer">
-            <div class="SiteSearchPanel__SearchBlock__Inner__Logo icon-delivery-logo"> </div>
-            <h1 class="SiteSearchPanel__SearchBlock__Inner__Title">Life's too short to miss out.</h1>
+        <div className="SiteSearchPanel__SearchBlock__Inner">
+          <div className="SiteSearchPanel__SearchBlock__OpacityCover"> </div>
+          <div className="SiteSearchPanel__SearchBlock__OpacityContainer">
+            <div className="SiteSearchPanel__SearchBlock__Inner__Logo icon-delivery-logo"> </div>
+            <h1 className="SiteSearchPanel__SearchBlock__Inner__Title">Life's too short to miss out.</h1>
             <div>
-              <form class="SiteSearchPanel__SearchBlock__Inner__Autocomplete">
-                <div class="AddressAutocomplete">
-                  <div class="AutocompleteInput">
-                    <input class="AutocompleteInput__input" placeholder="What city would you like to experience?" />
+              <form className="SiteSearchPanel__SearchBlock__Inner__Autocomplete">
+                <div className="AddressAutocomplete">
+                  <div className="AutocompleteInput">
+                    <input className="AutocompleteInput__input" placeholder="What city would you like to experience?" />
                   </div>
                 </div>
-                <button type="submit" class="Button">
+                <button type="submit" className="Button">
                   <div>
                     <span>Discover Now !! </span>
                   </div>
