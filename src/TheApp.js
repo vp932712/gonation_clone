c7261b9c2c92a6859ceccaaf31fa53d6ed00b333
Vic Patel <patel92vaibhav@gmail.com>
2018-05-20 15:50:26 -0400

    0.2.3

--- src/TheApp.js
+++ src/TheApp.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import "./DownloadTheApp.css"
-
-
+import first from'./images/1.png';
+import second from'./images/2.png';
+import third from'./images/3.png';
+import apple from './images/apple.png'
+import android from './images/android.png'
 
 class TheApp extends Component {
 
@@ -10,19 +13,19 @@ render() {
     <div class="MobileAppPanel">
     <div class="MobileAppPanel__Container">
       <div class="MobileAppPanel__Layers">
-        <img />
-        <img />
-        <img />
+        <img class="MobileAppPanel__Layers__Layer" src={first} />
+        <img class="MobileAppPanel__Layers__Layer_2" src={second}/>
+        <img class="MobileAppPanel__Layers__Layer_3" src={third}/>
       </div>
-      <div>
-        <h1> </h1>
-        <p> </p>
-        <div>
-          <a>
-            <img />
+      <div class="MobileAppForm">
+        <h1 class="MobileAppForm__Title">Tap into the pulse. </h1>
+        <p class="MobileAppForm__Tagline">Always know what’s happening at your favorite places, or discover something amazing in the moment. </p>
+        <div class="MobileAppForm__Buttons">
+          <a class="MobileAppForm__Buttons__Button">
+            <img class="MobileAppForm__Buttons__Button__Image" src={apple} />
           </a>
-          <a>
-            <img />
+          <a class="MobileAppForm__Buttons__Button">
+            <img class="MobileAppForm__Buttons__Button__Image" src={android} />
           </a>
         </div>
       </div>
