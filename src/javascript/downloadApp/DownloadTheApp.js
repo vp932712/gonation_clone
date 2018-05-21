26a2018b185638e78dc2a8dfc0f17e49d81506a3
Vic Patel <patel92vaibhav@gmail.com>
2018-05-20 23:56:16 -0400

    0.5.1

--- src/javascript/downloadApp/DownloadTheApp.js
+++ src/javascript/downloadApp/DownloadTheApp.js
@@ -3,23 +3,20 @@ import "../../css/DownloadTheApp.css"
 import "../../css/WhoWeAre.css"
 import TheApp from "./TheApp.js"
 
-
 class DownloadTheApp extends Component {
 
 render() {
   return (
     <div>
-    <div className="Main__Us">
-      <h2 className="Light">Every city is alive</h2>
-      <div className="HomepageDivider"> </div>
-      <TheApp />
+      <div className="Main__Us">
+        <h2 className="Light">Every city is alive</h2>
+        <div className="HomepageDivider"> </div>
+          <TheApp />
+        </div>
     </div>
-
-    </div>
-  )
+   );
+  }
 }
 
-}
 
-
-export default DownloadTheApp
+export default DownloadTheApp;
--- src/javascript/downloadApp/TheApp.js
+++ src/javascript/downloadApp/TheApp.js
@@ -11,31 +11,29 @@ class TheApp extends Component {
 render() {
   return (
     <div className="MobileAppPanel">
-    <div className="MobileAppPanel__Container">
-      <div className="MobileAppPanel__Layers">
-        <img className="MobileAppPanel__Layers__Layer" src={first} />
-        <img className="MobileAppPanel__Layers__Layer_2" src={second}/>
-        <img className="MobileAppPanel__Layers__Layer_3" src={third}/>
-      </div>
-      <div className="MobileAppForm">
-        <h1 className="MobileAppForm__Title">Tap into the pulse </h1>
-        <p className="MobileAppForm__Tagline">Always know what’s happening at your favorite places, or discover something amazing in the moment. </p>
-        <div className="MobileAppForm__Buttons">
-          <a className="MobileAppForm__Buttons__Button">
-            <img className="MobileAppForm__Buttons__Button__Image" src={apple} />
-          </a>
-          <a className="MobileAppForm__Buttons__Button">
-            <img className="MobileAppForm__Buttons__Button__Image" src={android} />
-          </a>
+      <div className="MobileAppPanel__Container">
+        <div className="MobileAppPanel__Layers">
+          <img className="MobileAppPanel__Layers__Layer" src={first} />
+          <img className="MobileAppPanel__Layers__Layer_2" src={second}/>
+          <img className="MobileAppPanel__Layers__Layer_3" src={third}/>
         </div>
-      </div>
-
+        <div className="MobileAppForm">
+          <h1 className="MobileAppForm__Title">Tap into the pulse </h1>
+          <p className="MobileAppForm__Tagline">Always know what’s happening at your favorite places, or discover something amazing in the moment. </p>
+          <div className="MobileAppForm__Buttons">
+            <a className="MobileAppForm__Buttons__Button">
+              <img className="MobileAppForm__Buttons__Button__Image" src={apple} />
+            </a>
+            <a className="MobileAppForm__Buttons__Button">
+              <img className="MobileAppForm__Buttons__Button__Image" src={android} />
+            </a>
+          </div>
+         </div>
+       </div>
     </div>
-    </div>
-  )
+   );
+  }
 }
 
-}
 
-
-export default TheApp
+export default TheApp;
